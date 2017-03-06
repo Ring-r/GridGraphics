889c95aa95dc3d6a9cfc2274adadaa0f31e928cb
Ring_r <Rostyslav.Pudlo@gmail.com>
2017-03-06 17:46:26 +0200

    Temporary

---
+++ GridGraphics/Grid.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Drawing;
+
+namespace GridGraphics
+{
+    class Grid
+    {
+        public class AxisSettings
+        {
+            private int count;
+            private float step = 1;
+            private float size;
+
+            private float anchorCoef;
+            private float anchorShift;
+
+            private float anchorViewCoef;
+            private float anchorViewShift;
+            private float viewSize;
+
+            private void RecalculateAnchor(bool anchorAsCoef)
+            {
+                if (anchorAsCoef)
+                    ; // TODO: Recalculate anchorShift;
+                else
+                    ; // TODO: Recalculate anchorCoef;
+            }
+
+            private void RecalculateAnchorView(bool anchorAsCoef)
+            {
+                if (anchorAsCoef)
+                    ; // TODO: Recalculate anchorViewShift;
+                else
+                    ; // TODO: Recalculate anchorViewCoef;
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return count;
+                }
+            }
+            public void SetCount(int value, bool anchorAsCoef)
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+
+                count = value;
+
+                RecalculateAnchor(anchorAsCoef);
+            }
+            public void SetStep(float value, bool anchorAsCoef)
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException();
+
+                if (step == value)
+                    return;
+
+                step = value;
+
+                RecalculateAnchor(anchorAsCoef);
+            }
+
+            public float Size
+            {
+                get
+                {
+                    return count * step;
+                }
+            }
+
+            public float AnchorCoef
+            {
+                get
+                {
+                    return anchorCoef;
+                }
+                set
+                {
+                    anchorCoef = value;
+
+                    RecalculateAnchor(true);
+                }
+            }
+            public float AnchorShift
+            {
+                get
+                {
+                    return anchorShift;
+                }
+                set
+                {
+                    anchorShift = value;
+
+                    RecalculateAnchor(false);
+                }
+            }
+
+            public float AnchorViewCoef
+            {
+                get
+                {
+                    return anchorViewCoef;
+                }
+                set
+                {
+                    anchorViewCoef = value;
+
+                    RecalculateAnchorView(true);
+                }
+            }
+            public float AnchorViewShift
+            {
+                get
+                {
+                    return anchorViewShift;
+                }
+                set
+                {
+                    anchorViewShift = value;
+
+                    RecalculateAnchorView(false);
+                }
+            }
+            public float ViewSize
+            {
+                get
+                {
+                    return viewSize;
+                }
+            }
+            public void SetViewSize(float value, bool anchorViewAsCoef)
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException();
+
+                if (viewSize == value)
+                    return;
+
+                viewSize = value;
+
+                RecalculateAnchorView(anchorViewAsCoef);
+            }
+
+            public void MoveByCell(int shift)
+            {
+                if (shift == 0 || Count == 0)
+                    return;
+
+                AnchorCoef += (float)shift / Count;
+            }
+
+            public void MoveByView(float shift)
+            {
+                if (shift == 0)
+                    return;
+
+                anchorViewShift += shift / ViewSize;
+            }
+        }
+
+        public readonly AxisSettings AxisSettingsX = new AxisSettings();
+        public readonly AxisSettings AxisSettingsY = new AxisSettings();
+
+        public Grid()
+        {
+            AxisSettingsX.SetCount(10, true);
+            AxisSettingsY.SetCount(10, true);
+        }
+
+        private float viewSizeX;
+
+        private float viewSizeY;
+
+        private float step = 1;
+        public float Step
+        {
+            get
+            {
+                return step;
+            }
+        }
+        public void SetStep(float value, bool anchorAsCoefX, bool anchorAsCoefY)
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (step == value)
+                return;
+
+            step = value;
+
+            AxisSettingsX.SetStep(step, anchorAsCoefX);
+            AxisSettingsY.SetStep(step, anchorAsCoefY);
+        }
+
+        public void ScaleToFit()
+        {
+            if (AxisSettingsX.Count == 0 || AxisSettingsY.Count == 0)
+                return;
+
+            var step = Math.Min(viewSizeX / AxisSettingsX.Count, viewSizeY / AxisSettingsY.Count);
+            AxisSettingsX.SetStep(step, true); // TODO: Is it correct?
+            AxisSettingsY.SetStep(step, true); // TODO: Is it correct?
+        }
+
+        private readonly Pen pen = Pens.Black;
+        public void Draw(Graphics graphics)
+        {
+            // TODO: Find correct start value closed to min.
+
+            var minX = AxisSettingsX.AnchorViewShift - AxisSettingsX.AnchorShift;
+            var maxX = minX + AxisSettingsX.Size;
+            var minY = AxisSettingsY.AnchorViewShift - AxisSettingsY.AnchorShift;
+            var maxY = minY + AxisSettingsY.Size;
+
+            var step = Step;
+            for (var x = minX; x < maxX - step / 2; x += step)
+            {
+                for (var y = minY; y < maxY - step / 2; y += step)
+                    graphics.DrawRectangle(pen, x, y, step, step);
+            }
+        }
+    }
+}
--- GridGraphics/MainForm.cs
+++ GridGraphics/MainForm.cs
@@ -6,136 +6,7 @@ namespace GridGraphics
 {
     public partial class MainForm : Form
     {
-        private class Grid
-        {
-            private int iCount = 10;
-            private int jCount = 10;
-
-            private float gridCoefX;
-            private float gridSizeX
-            {
-                get
-                {
-                    return Step * iCount;
-                }
-            }
-            private float viewCoefX;
-            private float viewSizeX;
-            public float ViewSizeX
-            {
-                get
-                {
-                    return viewSizeX;
-                }
-                set
-                {
-                    if (value < 0)
-                        throw new ArgumentException();
-
-                    viewSizeX = value;
-                }
-            }
-
-            private float gridCoefY;
-            private float gridSizeY
-            {
-                get
-                {
-                    return Step * jCount;
-                }
-            }
-            private float viewCoefY;
-            private float viewSizeY;
-            public float ViewSizeY
-            {
-                get
-                {
-                    return viewSizeY;
-                }
-                set
-                {
-                    if (value < 0)
-                        throw new ArgumentException();
-
-                    viewSizeY = value;
-                }
-            }
-
-            public void SetAnchor(float viewCoefX, float gridCoefX, float viewCoefY, float gridCoefY)
-            {
-                this.viewCoefX = viewCoefX;
-                this.gridCoefX = gridCoefX;
-
-                this.viewCoefY = viewCoefY;
-                this.gridCoefY = gridCoefY;
-            }
-
-            private float step = 1;
-            public float Step
-            {
-                get
-                {
-                    return step;
-                }
-                set
-                {
-                    if (value <= 0)
-                        return;
-
-                    if (step == value)
-                        return;
-
-                    step = value;
-                }
-            }
-
-            public void ScaleToFit()
-            {
-                if (iCount == 0 || jCount == 0)
-                    return;
-
-                Step = Math.Min(viewSizeX / iCount, viewSizeY / jCount);
-            }
-
-            public void MoveByCell(int di, int dj)
-            {
-                if (iCount <= 0 || jCount <= 0)
-                    return;
-
-                gridCoefX += (float)di / iCount;
-                gridCoefY += (float)dj / jCount;
-            }
-
-            public void MoveByView(float dx, float dy)
-            {
-                if (viewSizeX == 0 || viewSizeY == 0)
-                    return;
-
-                viewCoefX += dx / ViewSizeX;
-                viewCoefY += dy / ViewSizeY;
-            }
-
-            private readonly Pen pen = Pens.Black;
-            public void Draw(Graphics graphics)
-            {
-                // TODO: Find correct start value closed to min.
-
-                var minX = viewCoefX * viewSizeX - gridCoefX * gridSizeX;
-                var maxX = minX + gridSizeX;
-                var minY = viewCoefY * viewSizeY - gridCoefY * gridSizeY;
-                var maxY = minY + gridSizeY;
-
-                var step = Step;
-                for (var x = minX; x < maxX - step / 2; x += step)
-                {
-                    for (var y = minY; y < maxY - step / 2; y += step)
-                        graphics.DrawRectangle(pen, x, y, step, step);
-                }
-            }
-        }
-
         private readonly Grid grid = new Grid();
-
 
         public MainForm()
         {
@@ -145,16 +16,18 @@ namespace GridGraphics
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
         {
             var di = 0;
+            if (e.KeyCode == Keys.Left)
+                di += 1;
+            if (e.KeyCode == Keys.Right)
+                di -= 1;
+            grid.AxisSettingsX.MoveByCell(di);
+
             var dj = 0;
             if (e.KeyCode == Keys.Up)
                 dj += 1;
-            if (e.KeyCode == Keys.Right)
-                di -= 1;
             if (e.KeyCode == Keys.Down)
                 dj -= 1;
-            if (e.KeyCode == Keys.Left)
-                di += 1;
-            grid.MoveByCell(di, dj);
+            grid.AxisSettingsY.MoveByCell(di);
 
             Invalidate();
         }
@@ -165,7 +38,10 @@ namespace GridGraphics
                 Close();
             if (e.KeyCode == Keys.Enter)
             {
-                grid.SetAnchor(0.5f, 0.5f, 0.5f, 0.5f);
+                grid.AxisSettingsX.AnchorCoef = 0.5f;
+                grid.AxisSettingsX.AnchorViewCoef = 0.5f;
+                grid.AxisSettingsY.AnchorCoef = 0.5f;
+                grid.AxisSettingsY.AnchorViewCoef = 0.5f;
                 grid.ScaleToFit();
 
                 Invalidate();
@@ -179,8 +55,8 @@ namespace GridGraphics
 
         private void MainForm_Resize(object sender, EventArgs e)
         {
-            grid.ViewSizeX = ClientSize.Width;
-            grid.ViewSizeY = ClientSize.Height;
+            grid.AxisSettingsX.SetViewSize(ClientSize.Width, true);
+            grid.AxisSettingsY.SetViewSize(ClientSize.Height, true);
         }
 
         private Point mouseLocation;
@@ -194,7 +70,8 @@ namespace GridGraphics
             if (e.Button != MouseButtons.Left)
                 return;
 
-            grid.MoveByView(e.X - mouseLocation.X, e.Y - mouseLocation.Y);
+            grid.AxisSettingsX.MoveByView(e.X - mouseLocation.X);
+            grid.AxisSettingsY.MoveByView(e.Y - mouseLocation.Y);
 
             mouseLocation = e.Location;
 
@@ -203,8 +80,8 @@ namespace GridGraphics
 
         private void MainForm_MouseWheel(object sender, MouseEventArgs e)
         {
-            const int WHEEL_DELTA = 120; // TODO: How get it from system.
-            grid.Step -= e.Delta / WHEEL_DELTA;
+            const int WHEEL_DELTA = 120; // TODO: How get it from system?
+            grid.SetStep(grid.Step - e.Delta / WHEEL_DELTA, true, true);
 
             Invalidate();
         }
